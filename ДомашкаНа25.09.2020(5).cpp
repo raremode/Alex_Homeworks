fb56ce00112738bcdf265094aed34236eccb1d05
raremode <70842951+raremode@users.noreply.github.com>
2020-09-21 12:00:20 +0300

    Create ДомашкаНа25.09.2020(5).cpp

---
+++ ДомашкаНа25.09.2020(5).cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int x, y, z, u, v;
+	printf("Введите параметры кирпича : Длина, Ширина, Высота \n");
+	scanf_s("%d %d %d", &x, &y, &z);
+	printf("Введите параметры дырки:Длина и Ширина \n");
+	scanf_s("%d %d", &u, &v);
+	if (x <= u || y <= u || z <= u || x <= v || y <= v || z <= v) printf("Кирпич войдет в дырку \n");
+	else printf("Кирпич не войдет в дырку \n");
+}
