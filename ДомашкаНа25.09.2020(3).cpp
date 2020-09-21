125e10757fa6e124e87eb44996230cb75d8c5600
raremode <70842951+raremode@users.noreply.github.com>
2020-09-21 12:51:43 +0300

    Create ДомашкаНа25.09.2020(3).cpp

---
+++ ДомашкаНа25.09.2020(3).cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main() {
+    int n, a, b(0);
+    setlocale(LC_ALL, "RUS");
+    scanf_s("%d", &n);
+    if (n > 0 && n < 5) {
+        for (int i = 0; i < n; i++) {
+            scanf_s("%d", &a);
+            b += a;
+        }
+        printf("Сумма чисел равна %d", b);
+    }
+    else {
+        printf("Введите заново число от 0 до 5");
+    }
+    return 0;
+}
