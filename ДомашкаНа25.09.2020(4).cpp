89e1608216577a6701644fa558179cc3168b1a70
raremode <70842951+raremode@users.noreply.github.com>
2020-09-21 12:55:37 +0300

    Create ДомашкаНа25.09.2020(4).cpp

---
+++ ДомашкаНа25.09.2020(4).cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, a, b;
+    scanf_s("%d %d", &a, &b);
+    for (int i = 0; i <= a;) {
+        printf("%d \n", i);
+        i += b;
+    }
+    return 0;
+}
